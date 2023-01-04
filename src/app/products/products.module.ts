f500e874f32c1c743dfd3382c20186ad9c2bb46b
Akshaya Manickavasagam <akshayamanickavasagam98@gmail.com>
2023-01-05 00:36:48 +0530

    products saved

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule} from  '@angular/common/http';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
+import { FormsModule } from '@angular/forms';
+import { ProductsService } from './products.service';
+import { ProductViewComponent } from '../product-view/product-view.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    ProductViewComponent
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    FormsModule,
+    HttpClientModule,
+   
+  ],
+  providers:[
+    ProductsService
   ]
 })
 export class ProductsModule { }
